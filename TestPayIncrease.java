66fc64a48386feb99383eaf4a189e46c9a2b40ff
Tihomir Ilic <enlighter.ilic@gmail.com>
2018-04-03 16:20:11 +0200

    Add files via upload

---
+++ TestPayIncrease.java
@@ -0,0 +1,36 @@
+import com.practicaljava.lesson6.*;
+
+public class TestPayIncrease {
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Person workers[] = new Person[3];
+		workers[0] = new Employee("Johny");
+		workers[1] = new Employee("Mary");
+		workers[2] = new Employee("Steve");
+		
+		 
+		Employee currentEmployee;
+		Contractor currentContractor;
+		
+		
+		for(Person p: workers) {
+			if(p instanceof Employee) {
+				currentEmployee = (Employee) p;
+				currentEmployee.increasePay(30);
+				
+			}
+			else if (p instanceof Contractor) {
+				currentContractor = (Contractor) p;
+				currentContractor.increasePay(30);
+				
+			}	
+			
+		}
+		
+		}
+	
+}
